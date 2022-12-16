7a7f9b9829c92d763d49c1a10003972069d930e4
Zuhaib Amjad <umar0423@gmail.com>
2022-12-16 13:45:14 +0500

    Add class MyList to the projecyt

---
+++ class.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative 'enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each
+    yield @list
+  end
+end
+
+list1 = MyList.new(1, 2, 3, 4)
+puts list1.all? { |e| e < 5 }
+puts list1.any? { |e| e == 2 }
+puts list1.filter(&:even?)
