9d2d73814d332c8f5d44884133f55e817e988d38
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2022-12-16 10:21:27 +0100

    Update enumerable.rb module

--- enumerable.rb
+++ enumerable.rb
@@ -1,21 +1,21 @@
 module MyEnumerable
-    def all?
-        new_arr = []
-        @list.each { |item| new_arr.push(item) if yield item }
-        puts new_arr.length == @list.length
-    end
+  def all?
+    new_arr = []
+    @list.each { |item| new_arr.push(item) if yield item }
+    puts new_arr.length == @list.length
+  end
 
-    def any?
-        @list.each { |item| return true if yield item }
-    end
+  # any? method
+  def any?
+    @list.each { |item| return true if yield item }
+  end
 
-    def filter
-        arr = []
-        @list.each do |item|
-            arr.push(item) if yield item
-        end
-        print arr
+  # filter method
+  def filter
+    arr = []
+    @list.each do |item|
+      arr.push(item) if yield item
     end
+    print arr
+  end
 end
-
-
