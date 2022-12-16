1d7bfe85cbd24a08ce9a23f5b5431b2eabf5b626
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2022-12-16 10:07:06 +0100

    Add MyEnumerable module, all, any & filter methods

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,21 @@
-# frozen_string_literal: true
+module MyEnumerable
+    def all?
+        new_arr = []
+        @list.each { |item| new_arr.push(item) if yield item }
+        puts new_arr.length == @list.length
+    end
+
+    def any?
+        @list.each { |item| return true if yield item }
+    end
+
+    def filter
+        arr = []
+        @list.each do |item|
+            arr.push(item) if yield item
+        end
+        print arr
+    end
+end
 
 
